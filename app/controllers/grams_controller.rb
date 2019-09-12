37f078a79530ee569537aa62c3d1fd4f0d6fd964
Erny Juarez <ernyjuarez@gmail.com>
2019-09-12 03:02:56 +0000

    added CarrierWave

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -27,6 +27,7 @@ class GramsController < ApplicationController
   end
 
 	def index
+		@grams = Gram.all
 	end 
 
   def show
@@ -54,11 +55,11 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
     render plain: "#{status.to_s.titleize} :(", status: status
   end
-  
+
 end
